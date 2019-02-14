27f278ebb774c59508b7ecec9da227fa68cc2f6d
Jack Chen <jackjchen@gmail.com>
2019-02-14 12:56:32 -0600

    Revert to older apr tools

--- httpd/build.sh
+++ httpd/build.sh
@@ -2,9 +2,9 @@
 
 # Download APR
 mkdir -p $SRC_DIR/srclib/apr
-curl https://archive.apache.org/dist/apr/apr-1.6.5.tar.gz | tar -xz -C $SRC_DIR/srclib/apr --strip-components=1
+curl https://archive.apache.org/dist/apr/apr-1.5.2.tar.gz | tar -xz -C $SRC_DIR/srclib/apr --strip-components=1
 mkdir -p $SRC_DIR/srclib/apr-util
-curl https://archive.apache.org/dist/apr/apr-util-1.6.1.tar.gz | tar -xz -C $SRC_DIR/srclib/apr-util --strip-components=1
+curl https://archive.apache.org/dist/apr/apr-util-1.5.4.tar.gz | tar -xz -C $SRC_DIR/srclib/apr-util --strip-components=1
 
 ./configure --help
 
