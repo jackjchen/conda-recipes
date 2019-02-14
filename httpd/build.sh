97c0d28451bd6fe9abccacc1f75b3d914420e559
Jack Chen <j.chen1@partner.samsung.com>
2019-02-14 15:14:33 -0600

    Allow curl insecure download

--- httpd/build.sh
+++ httpd/build.sh
@@ -2,9 +2,9 @@
 
 # Download APR
 mkdir -p $SRC_DIR/srclib/apr
-curl https://archive.apache.org/dist/apr/apr-1.5.2.tar.gz | tar -xz -C $SRC_DIR/srclib/apr --strip-components=1
+curl -k https://archive.apache.org/dist/apr/apr-1.5.2.tar.gz | tar -xz -C $SRC_DIR/srclib/apr --strip-components=1
 mkdir -p $SRC_DIR/srclib/apr-util
-curl https://archive.apache.org/dist/apr/apr-util-1.5.4.tar.gz | tar -xz -C $SRC_DIR/srclib/apr-util --strip-components=1
+curl -k https://archive.apache.org/dist/apr/apr-util-1.5.4.tar.gz | tar -xz -C $SRC_DIR/srclib/apr-util --strip-components=1
 
 ./configure --help
 
